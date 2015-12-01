565ef6a61e5321cc5a11ea0d2017c9b27ee3a73c
steve.brazier <steve.brazier@just-eat.com>
2015-12-01 16:02:44 +0000

    clean up of day one part two

--- lib/code_advent/day_one/part_two.ex
+++ lib/code_advent/day_one/part_two.ex
@@ -10,19 +10,29 @@ defmodule CodeAdvent.DayOne.PartTwo do
       |> Stream.map(&instruction_to_floor_change/1)
       |> Stream.with_index
       |> Stream.scan({0 , nil}, &update_floor/2)
-      |> Stream.take_while(fn {floor, _} -> floor !== @target_floor end)
-      |> get_last
-      |> add_one # index is zero based
-      |> add_one # stream stops 1 before the floor is reached
-      |> answer_as_string
+      |> run_up_to_floor(@target_floor)
+      |> extract_answer
   end
 
   defp instruction_to_floor_change("("), do: 1
   defp instruction_to_floor_change(")"), do: -1
   defp instruction_to_floor_change(_), do: 0
 
-  defp update_floor({change, n}, {floor, _}) do
-    {floor + change, n}
+  defp update_floor({change, n}, {previous_floor, _}) do
+    {previous_floor + change, n}
+  end
+
+  defp run_up_to_floor(stream, floor_target) do
+    stream
+      |> Stream.take_while(fn {floor, _} -> floor !== floor_target end)
+  end
+
+  defp extract_answer(stream) do
+    stream
+      |> get_last
+      |> add_one # index is zero based
+      |> add_one # stream stops 1 before the floor is reached
+      |> answer_as_string
   end
 
   defp answer_as_string({_, move_number}), do: "#{move_number}"
