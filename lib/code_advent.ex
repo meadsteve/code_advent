a8ad0b106ac9755df21c6df3af3cb03becce4fd7
steve.brazier <steve.brazier@just-eat.com>
2015-12-01 11:51:47 +0000

    only run one day

--- lib/code_advent.ex
+++ lib/code_advent.ex
@@ -1,6 +1,7 @@
 [
   {"Day one:", CodeAdvent.DayOne.Solve}
 ]
-  |> Enum.map(fn {intro, module} -> {intro, module.run()} end)
-  |> Enum.map(fn {intro, result} -> intro <> " " <> result end)
-  |> Enum.each(&IO.puts/1)
+  |> Stream.map(fn {intro, module} -> {intro, module.run()} end)
+  |> Stream.map(fn {intro, result} -> intro <> " " <> result end)
+  |> Stream.each(&IO.puts/1)
+  |> Enum.take 1
