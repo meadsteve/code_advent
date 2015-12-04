1690ad7c04192595e185d8d8e61e7e65e478faa6
steve.brazier <steve.brazier@just-eat.com>
2015-12-04 15:36:15 +0000

    single threaded simplier filter

--- lib/code_advent/day_four/part_one.ex
+++ lib/code_advent/day_four/part_one.ex
@@ -1,45 +1,30 @@
 defmodule CodeAdvent.DayFour.PartOne do
 
   @batch_size 500
+  @max_attempt 100000000
 
   def run(), do: run("iwrupvqb", seed: 0, target_zeros: 5)
 
-  def run(input, seed: x, target_zeros: target_zeros) do
-    input
-      |> loop(seed: x, target_zeros: target_zeros)
+  def run(input, seed: seed, target_zeros: target_zeros) do
+    goal_start_string = target_zeros |> as_zeros
+
+    [{answer, _}] = seed..@max_attempt
+     |> Stream.map(fn(n) -> {n, md5("#{input}#{n}")} end)
+     |> Stream.filter(&starts_with?(&1, goal_start_string))
+     |> Enum.take(1)
+
+    answer
       |> answer_as_string
   end
 
-  defp loop(string, seed: x, target_zeros: target_zeros) do
-    goal_string = 1 .. target_zeros
+  defp as_zeros(target_zeros) do
+    1 .. target_zeros
       |> Enum.map(fn _ -> "0" end)
       |> Enum.join
-    loop(string, x, goal_string)
   end
 
-  defp loop(string, value, goal_start_string) do
-    receive do
-      {:answer, value}  -> value
-     after
-       0_001 ->
-         check_values(string, value .. (value + @batch_size), goal_start_string)
-         loop(string, value + @batch_size, goal_start_string)
-     end
-  end
-
-  defp check_values(string, values, goal_start_string) do
-    resond_to = self
-    spawn_link fn ->
-      Enum.each(values, &check_md5(&1, string, goal_start_string, resond_to))
-    end
-    send resond_to, {:nope}
-  end
-
-  defp check_md5(value, string, goal_start_string, resond_to) do
-    hash = md5("#{string}#{value}")
-    if hash |> String.starts_with?(goal_start_string) do
-      send resond_to, {:answer, value}
-    end
+  defp starts_with?({_, md5_string}, goal_start_string) do
+    String.starts_with?(md5_string, goal_start_string)
   end
 
   # md5 implementation stolen from
