0da5284f051f7a0ada106b486f619348e9f524c2
steve.brazier <steve.brazier@just-eat.com>
2015-12-10 10:05:15 +0000

    tidy up

--- lib/code_advent/day_ten/part_one.ex
+++ lib/code_advent/day_ten/part_one.ex
@@ -4,7 +4,7 @@ defmodule CodeAdvent.DayTen.PartOne do
     "3113322113"
       |> run(times: 40)
       |> String.length
-      |> as_string 
+      |> as_string
   end
 
   def run(string, times: 0), do: string
@@ -15,16 +15,18 @@ defmodule CodeAdvent.DayTen.PartOne do
   end
 
   def convert(number_string) do
-    convert(String.graphemes(number_string), 1, "")
+    number_string
+      |> String.graphemes
+      |> convert(1, "")
   end
 
-  def convert([], _, output), do: output
+  defp convert([], _, output), do: output
 
-  def convert([n, n | rest], count, output) do
+  defp convert([n, n | rest], count, output) do
     convert([n | rest], count + 1, output)
   end
 
-  def convert([n | rest], count, output) do
+  defp convert([n | rest], count, output) do
     convert(rest, 1, output <> as_string(count) <> n)
   end
 
