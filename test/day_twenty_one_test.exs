1f582f0975d3fbc01655e7553f2c2b62efb86339
steve.brazier <meadsteve@gmail.com>
2015-12-26 14:58:00 +0000

    add game playing for day 21

--- test/day_twenty_one_test.exs
+++ test/day_twenty_one_test.exs
@@ -26,6 +26,51 @@ defmodule DayTwentyOneTest do
     assert updated_defender.hp == 99
   end
 
+  test "Dead people's attacks do no damage" do
+    defender = %PartOne.Person{hp: 100, armor: 5}
+    attacker = %PartOne.Person{damage: 10, hp: 0}
+
+    updated_defender = defender |> PartOne.is_attacked_by(attacker)
+    assert updated_defender.hp == 100
+  end
+
+  test "A game turn causes the player to attack then the boss to return" do
+    updated_game = PartOne.Game.new
+      |> PartOne.turn
+    assert updated_game.player.hp == 92 # Boss has hit player
+  end
+
+  test "A game is over when either the player or boss is dead" do
+    new_game = PartOne.Game.new
+    dead_player_game = new_game
+      |> Map.put(:player, %PartOne.Person{hp: 0})
+    dead_boss_game = new_game
+      |> Map.put(:boss, %PartOne.Person{hp: 0})
+
+    assert PartOne.is_over?(dead_player_game) == true
+    assert PartOne.is_over?(dead_boss_game) == true
+    assert PartOne.is_over?(new_game) == false
+  end
+
+  test "The default game runs until the player is dead" do
+    dead_player_game = PartOne.Game.new
+      |> PartOne.play_out
+
+    assert PartOne.is_over?(dead_player_game) == true
+    assert dead_player_game.player.hp <= 0
+  end
+
+  test "The winner of the game can be determined" do
+    new_game = PartOne.Game.new
+    dead_player_game = new_game
+      |> Map.put(:player, %PartOne.Person{hp: 0})
+    dead_boss_game = new_game
+      |> Map.put(:boss, %PartOne.Person{hp: 0})
+
+    assert PartOne.winner(dead_player_game) == :boss
+    assert PartOne.winner(dead_boss_game) == :player
+  end
+
   test "Buying items increases money spent" do
     item_cost = 10
     item = {:ring, "Item", item_cost, 0, 0}
